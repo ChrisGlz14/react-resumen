369ce527e27d3eb656fd2d384c9b5c2b3a5686b8
ChrisGlz14 <christiang14@hotmail.com.ar>
2023-10-16 19:14:03 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { PrimerComponente } from './components/PrimerComponenteProps'
+
+function App() {
+
+  return (
+    <>
+      <PrimerComponente titulo = "ESTE ES UNA PROPS" subtitulo="Curso de Sergi CODE" numero = {7}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PrimerComponenteProps.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// rafc para poder crear el componente que se exporte solo
+
+export const PrimerComponente = ( {titulo, subtitulo, numero}) => {
+ 
+    return (
+        <>
+        <h1>{titulo}</h1>
+        <h2>{subtitulo}</h2>
+
+        <h2>2 + {numero}</h2>
+        
+        <h2>Resultado =  {2 + numero }</h2>
+        </>
+        )
+}
+
+// SIMULA TYPESCRIPT
+PrimerComponente.PropTypes = {
+
+    titulo: PropTypes.string.isRequired,
+    subtititulo : PropTypes.string.isRequired,
+    numero : PropTypes.number.isRequired
+
+}
+
+
+PrimerComponente.defaultProps = {
+
+    // Podrian HABER PROPS DEFAULT
+
+}
+
+
+// EVENTOS
